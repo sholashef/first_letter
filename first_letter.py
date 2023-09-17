8f1f571c68748812e537ea06d3012b31fd681c45
Oluwasola <113778568+sholashef@users.noreply.github.com>
2023-09-17 20:49:23 +0100

    Add files via upload
    Write a program named first_letter.py that prompts the user for input with the string "Tell me your password:". The program should then determine the first letter of the user’s input, convert that letter to uppercase, and display it back.

---
+++ first_letter.py
@@ -0,0 +1,8 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> prompt = ("Tell me your password: ")
+>>> user_password = input(prompt)
+Tell me your password: oluwasola
+>>> first_letter = user_password[0].upper()
+>>> print(first_letter)
+O
